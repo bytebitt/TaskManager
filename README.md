9835872837379b3cfc92bc222d00e8d048a0de5f
Byte Bit <Vetal201710@gmail.com>
2025-06-09 22:52:40 +0300

    update logic

---
+++ README.md
@@ -0,0 +1,19 @@
+# Terminal To-Do List
+
+A simple command-line To-Do List application written in Python.
+
+## Features
+
+- Add new tasks
+- Mark tasks as completed
+- Delete tasks from the list
+- Display the current list of tasks in the terminal
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/bytebitt/Term-To-Do-List
+cd Terminal To-Do List
+```
---
+++ main.py
@@ -0,0 +1,141 @@
+from typing import List
+from colorama import Fore, init, Style
+
+init(autoreset=True)
+
+tasks: List[str] = []
+
+
+def add_task() -> None:
+    """
+    Prompts the user to enter a new task and adds it to the global task list.
+
+    If the task already exists, the user is asked whether to add it anyway.
+
+    Returns:
+        None
+
+    """
+
+    task = input(f"{Fore.WHITE}Enter your task\n{Fore.BLUE}>>> ")
+
+    if not any(t['title'] == task for t in tasks):
+        tasks.append({"title": task, "done": False})
+
+        print(f"{Fore.GREEN}\nTask Successfully Added\n")
+    else:
+        print(
+            f"{Fore.YELLOW}This task is currently in the task list, add it anyway? (y/n)")
+
+        answer = input(f"{Fore.BLUE}>>> ")
+
+        if answer.lower() == 'y':
+            tasks.append({"title": task, "done": False})
+            print(f"\n{Fore.GREEN}Task Successfully Added\n")
+        elif answer.lower() == 'n':
+            print("...")
+
+
+def mark_task() -> None:
+    """
+    Prompts the user to enter a task number and sets its 'done' status to True.
+
+    If the task doesn't exist, the function shows an error warning.
+
+    Returns:
+        None
+    """
+
+    try:
+        display_tasks()
+        print('')
+        task = int(
+            input(f"{Fore.WHITE}Enter task number to mark it as completed\n{Fore.BLUE}>>> ")) - 1
+
+        if 0 <= task < len(tasks):
+            tasks[task]['done'] = True
+            print(f"{Fore.GREEN}\nTask successfully Marked\n")
+        else:
+            print(f"{Fore.RED}\nTask doesn't exist in your tasks list\n")
+    except ValueError:
+        print(f"{Fore.RED}\nIncorrect input\n")
+
+
+def delete_task() -> None:
+    """
+    Prompts the user to enter a task number and deletes the corresponding task from the global tasks list.
+
+    Displays the current list of tasks before prompting.
+
+    Handles invalid input (non-integer or out-of-range values) with error messages.
+
+    Returns:
+        None
+
+    """
+
+    try:
+        display_tasks()
+        print('')
+        task = int(
+            input(f"{Fore.WHITE}Enter task number to delete it\n{Fore.BLUE}>>> ")) - 1
+
+        if 0 <= task < len(tasks):
+            tasks.pop(task)
+            print(f"{Fore.GREEN}\nTask successfully deleted\n")
+        else:
+            print(f"{Fore.RED}\nTask doesn't exist in your tasks list\n")
+    except ValueError:
+        print(f"{Fore.RED}\nIncorrect input.\n")
+
+
+def display_tasks() -> None:
+    """
+    Displays all tasks in the global tasks list with their numbering.
+
+    If a task's 'done' status is True, its title is shown with a strikethrough effect.
+
+    If there are no tasks, prints a message indicating the list is empty.
+
+    Returns:
+        None
+    """
+
+    if tasks:
+        for i, task in enumerate(tasks, start=1):
+            title = task['title']
+            if task['done'] == True:
+                title = f"\x1b[9m{title}\x1b[0m"
+            print(f"{i}. {title}")
+    else:
+        print("List is Empty")
+
+
+def main():
+    while True:
+        print(f'{Fore.BLUE}-------- MENU --------')
+        print("1. Add Task")
+        print("2. Mark Task as Completed")
+        print("3. Delete Task")
+        print("4. Display all Task")
+        print("5. Exit")
+
+        choice = input(f"{Fore.BLUE}>>> {Fore.WHITE}")
+
+        match choice:
+            case '1':
+                add_task()
+            case '2':
+                mark_task()
+            case '3':
+                delete_task()
+            case '4':
+                display_tasks()
+            case '5':
+                break
+            case _:
+                print(f"{Fore.RED}\nIncorrect Input.\n")
+
+
+if __name__ == '__main__':
+    main()
